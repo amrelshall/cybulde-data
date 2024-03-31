34ad09afa5403349104eff3e38dc7839076efc48
amrelshall <amrelshall023@gmail.com>
2024-03-31 15:11:23 +0200

    added functionality to initialize dvc storage

--- cybulde/config_schemas/config_schema.py
+++ cybulde/config_schemas/config_schema.py
@@ -4,7 +4,9 @@ from pydantic.dataclasses import dataclass
 
 @dataclass
 class Config:
-    helle: str = "world"
+    dvc_remote_name: str = "gdrive"
+    dvc_remote_url: str = "gdrive://1dVLZt8La8m7HKLRRm8OFd9jGnWct3xMl/data/raw"
+    dvc_raw_data_folder: str = "data/raw"
 
 
 def setup_config() -> None:
